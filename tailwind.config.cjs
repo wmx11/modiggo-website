9f5b420d872c7e6f5587271891eb3637a7e9f95a
Vainius <vmykolaitis1@gmail.com>
2023-06-04 22:24:30 +0300

    First commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['[data-theme=light]'],
+          '--rounded-btn': '0',
+          '--rounded-box': '0',
+          '--tab-radius': '0',
+        },
+      },
+    ],
+  },
+};
