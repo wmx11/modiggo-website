e0db5c3fbcc712a787876101a16764a1bc65bbd2
Vainius <vmykolaitis1@gmail.com>
2023-06-16 13:14:05 +0300

    Removed unecessary iife wrapper

--- src/scripts/scrollTo.ts
+++ src/scripts/scrollTo.ts
@@ -1,21 +1,19 @@
-(() => {
-  document?.querySelectorAll(`[data-scroll-to]`).forEach((el) => {
-    const element = el as HTMLBaseElement;
+document?.querySelectorAll(`[data-scroll-to]`).forEach((el) => {
+  const element = el as HTMLBaseElement;
 
-    element?.addEventListener('click', () => {
-      const location = window.location.pathname;
-      if (location === '/') {
-        document
-          ?.querySelector(element?.dataset?.scrollTo as string)
-          ?.scrollIntoView({ behavior: 'smooth' });
-        window.history.pushState(
-          {},
-          '',
-          element?.dataset?.scrollTo as string
-        );
-      } else {
-        window.location.replace(`/${element?.dataset?.scrollTo as string}`);
-      }
-    });
+  element?.addEventListener('click', () => {
+    const location = window.location.pathname;
+    if (location === '/') {
+      document
+        ?.querySelector(element?.dataset?.scrollTo as string)
+        ?.scrollIntoView({ behavior: 'smooth' });
+      window.history.pushState(
+        {},
+        '',
+        element?.dataset?.scrollTo as string
+      );
+    } else {
+      window.location.replace(`/${element?.dataset?.scrollTo as string}`);
+    }
   });
-})();
+});``
